3e28204ef7a90e7987ebda86867a5e0faac22aed
Vladimir D <ande.vladimir@gmail.com>
2018-08-06 22:51:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,23 +1,22 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-function addElementToBeginningOfArray(array, element){
+function addElementToBeginningOfArray (array, element){
   return [element, ...array];
 }
-
-function destructivelyAddElementToBeginningOfArray(array,element){
+function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element);
   return array;
-} 
-function addElementToEndOfArray(array,element){
+}
+function addElementToEndOfArray (array,element){
   return [...array, element];
 }
-function destructivelyAddElementToEndOfArray(array,element){
+function destructivelyAddElementToEndOfArray(array, element){
   array.push(element);
   return array;
 }
-function accessElementInArray(array,index){
+function accessElementInArray (array, index){
   return array[index];
 }
-function destructivelyRemoveElementFromBeginningOfArray(array){
+function destructivelyRemoveElementFromBeginningOfArray (array){
   array.shift();
   return array;
 }
@@ -25,9 +24,13 @@ function removeElementFromBeginningOfArray(array){
   return array.slice(1);
 }
 function destructivelyRemoveElementFromEndOfArray(array){
-  array.pop();
+  array.pop(); 
   return array;
 }
 function removeElementFromEndOfArray(array){
-  return array.slice(0,array.length - 1);
+  return array.slice(0, array.length-1);
 }
+
+
+
+
